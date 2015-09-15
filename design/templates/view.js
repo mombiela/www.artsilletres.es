181a5887920fbd1236cba4969d82689661ef1284
VGCLI23 <VGCLI23@FIJCM01.central.fiatc.es>
2015-09-15 14:25:52 +0200

    Se añade nombre correcto para js

--- design/templates/view.js
+++ design/templates/view.js
@@ -8,14 +8,14 @@ $().ready(function() {
 
 function makeHeader()
 {
-	var uri = "$!uri";
+	var uri = "$!doc_name";
 	jQuery("#menu li").removeClass("current_page_item");
 	jQuery("#menu li:first").addClass("first");
 	
 	var list = jQuery("#menu li a");
 	for (var i = 0; i<list.length; i++)
 	{
-		if (list[i].href.indexOf("$uri")!=-1)
+		if (list[i].href.indexOf("$doc_name")!=-1)
 		{
 			console.log("Encontrado: " + list[i].href);
 			jQuery(list[i]).parent().addClass("current_page_item");
