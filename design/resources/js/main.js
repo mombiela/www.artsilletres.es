b56f7dcb22f73b60aa68b78bab4a2251431dcc88
VGCLI23 <VGCLI23@FIJCM01.central.fiatc.es>
2015-09-18 07:46:53 +0200

    Se mejora la visualización

--- design/resources/js/main.js
+++ design/resources/js/main.js
@@ -1,3 +1,7 @@
+// ------------------------------
+// Al iniciar realizamos cabecera
+// ------------------------------
+
 $().ready(function() {
     makeHeader();
 });
@@ -8,16 +12,24 @@ $().ready(function() {
 
 function makeHeader()
 {
-	var uri = "$!doc_name";
+	var uri = window.location.href;
+	
+	var i1 = uri.lastIndexOf(".html");
+	var i2 = uri.lastIndexOf("/");
+	if (i2 == -1) i2 = 0;
+	if (i1 == -1)	uri = "index";
+	else			uri = uri.substring(i1,i2);
+	
+	// Cambiamos estilos
 	jQuery("#menu li").removeClass("current_page_item");
 	jQuery("#menu li:first").addClass("first");
 	
+	// Añadimos current
 	var list = jQuery("#menu li a");
 	for (var i = 0; i<list.length; i++)
 	{
-		if (list[i].href.indexOf("$doc_name")!=-1)
+		if (list[i].href.indexOf(uri)!=-1)
 		{
-			console.log("Encontrado: " + list[i].href);
 			jQuery(list[i]).parent().addClass("current_page_item");
 		}
 	}
